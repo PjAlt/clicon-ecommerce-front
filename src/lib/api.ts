1fbc8f475f4827dd55c6f0b3fd54265f5b0b699a
prajjwal <prajjwaljohnny@gmail.com>
2025-06-13 21:07:37 +0545

    minor

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import { QueryClient } from '@tanstack/react-query';
 
-export const API_BASE_URL = 'https://localhost:7028';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const queryClient = new QueryClient({
   defaultOptions: {
